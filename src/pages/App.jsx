2e1ad797cb8bef96bfaa7edd079d635d85064160
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-08-05 11:51:41 -0300

    first commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+import Card from "../components/Card/Card.jsx";
+import Navbar from "../components/Navbar/Navbar.jsx";
+import SectionHeader from "../components/SectionHeader/SectionHeader.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <SectionHeader />
+      <Card />
+    </div>
+  );
+}
